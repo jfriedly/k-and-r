8a19159f905003515617e455fdcab1f26ee259cb
Joel Friedly <joelfriedly@gmail.com>
2015-05-23 20:13:37 -0700

    Exercise 1-2:  escape sequences in hello world

--- answers.rst
+++ answers.rst
@@ -8,4 +8,16 @@ It still works if you leave out the return statement (which I only added to sile
 
 .. code:: bash
 
-    git checkout 1-1; cc -Wall src/hello-world.c -o build/hello-world
+    git checkout 1-1; cc -Wall -std=c99 src/hello-world.c -o build/hello-world
+
+1-2
+---
+I started out by writing a loop that created a string containing ``\\c: \c`` for every lowercase character ``c``, but it turns out that this doesn't work.
+Apparently, the ``format`` argument to ``printf`` is evaluated at compile time, not run time.
+
+Instead, I generated the code with a bit of Python and pasted it into the C source file so that it would be there at compile time.
+One gets warnings about unknown escape sequences for most of them, errors for ``\u`` and ``\x`` because they're incomplete escape sequences, and some interesting output for the others.
+
+.. code:: bash
+
+    git checkout 1-2; cc -Wall -std=c99 src/hello-world.c -o build/hello-world
--- src/hello-world.c
+++ src/hello-world.c
@@ -1,8 +1,42 @@
 #include <stdio.h>
 
-
 int main(int argc, char **argv)
 {
 	printf("hello, world!\n");
+/*
+ * First version; doesn't work because printf format evaluation is
+ * apparently done at compile time:
+ * char s[9];
+ * for (int i='a'; i <= 'z'; i++) {
+ * 	sprintf(s, "\\\\%c: \\%c\n", i, i);
+ * 	printf(s);
+ * }
+ */
+	printf("\\a: \a\n");
+	printf("\\b: \b\n");
+	printf("\\c: \c\n");
+	printf("\\d: \d\n");
+	printf("\\e: \e\n");
+	printf("\\f: \f\n");
+	printf("\\g: \g\n");
+	printf("\\h: \h\n");
+	printf("\\i: \i\n");
+	printf("\\j: \j\n");
+	printf("\\k: \k\n");
+	printf("\\l: \l\n");
+	printf("\\m: \m\n");
+	printf("\\n: \n\n");
+	printf("\\o: \o\n");
+	printf("\\p: \p\n");
+	printf("\\q: \q\n");
+	printf("\\r: \r\n");
+	printf("\\s: \s\n");
+	printf("\\t: \t\n");
+	printf("\\u00bc (Unicode): \u00bc\n");
+	printf("\\v: \v\n");
+	printf("\\w: \w\n");
+	printf("\\x78 (hex): \x78\n");
+	printf("\\y: \y\n");
+	printf("\\z: \z\n");
 	return 0;
 }
